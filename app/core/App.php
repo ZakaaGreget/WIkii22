14a957af56ae610f76557d0b058b7683a154ac9a
ZakaaGreget <zakaashahzada3@gmail.com>
2024-11-24 15:59:37 +0700

    pusingnnn

--- app/core/App.php
+++ app/core/App.php
@@ -1,14 +1,26 @@
 <?php
 
 class App {
+
+    protected $controller = 'Home';
+    protected $method = 'index';
+    protected $params = [];
+
+
     public function __construct(){
         $url = $this->parseURL();
-        var_dump($url);
+        
+        if(file_exists('../app/controllers/' . $url[0] . '.php')){
+            $this->controller = $url[0];
+            unset($url[0]); 
+        }
     }
 
     public function parseURL(){
         if (isset($_GET['url'])){
-            $url = $_GET['url'];
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
             return $url;
         }
     }
