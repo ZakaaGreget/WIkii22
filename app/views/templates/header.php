bb85a68125dfe7fa8c1af1bbcdccecd14d2802ca
ZakaaGreget <zakaashahzada3@gmail.com>
2024-11-24 22:55:28 +0700

    add bootstrap dan css public

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -4,4 +4,5 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman <?= $data['judul']; ?> </title>
+    <link rel="stylesheet" href="http://localhost/WIKIIFIX/public/css/bootstrap.css">
 </head>
