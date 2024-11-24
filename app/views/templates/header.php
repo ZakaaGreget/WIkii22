fb22c2ccd91ee6adf902ce683fe8a334a606d16d
ZakaaGreget <zakaashahzada3@gmail.com>
2024-11-24 23:10:17 +0700

    rev

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -4,5 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman <?= $data['judul']; ?> </title>
-    <link rel="stylesheet" href="http://localhost/WIKIIFIX/public/css/bootstrap.css">
+    <link rel="stylesheet" href="http://localhost/Wikii22/WIKIIFIX/public/css/bootstrap.css">
+    
 </head>
