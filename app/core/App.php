336b3e026b414f3278dee3e6af739ddb867fdd3b
ZakaaGreget <zakaashahzada3@gmail.com>
2024-11-24 17:25:04 +0700

    controller and views

--- app/core/App.php
+++ app/core/App.php
@@ -14,8 +14,7 @@ class App {
         if( file_exists('../app/controllers/' . $url[0] . '.php')){
             $this->controller = $url[0];
             unset($url[0]); 
-            
-            
+
         }
         
         require_once '../app/controllers/' . $this->controller . '.php';
